7e003158a1cfdd1f9aed5f89377de7e845562337
zhouhaoyang1997 <zhouhaoyang1997@iCloud.com>
2019-08-19 16:24:47 +0800

    不想优化

--- src/main/java/com/zyzx/redbag/controller/PreClickController.java
+++ src/main/java/com/zyzx/redbag/controller/PreClickController.java
@@ -4,9 +4,12 @@ import com.zyzx.redbag.common.Const;
 import com.zyzx.redbag.entry.Result;
 import com.zyzx.redbag.entry.UserClick;
 import com.zyzx.redbag.service.PreClickService;
+import com.zyzx.redbag.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class PreClickController {
@@ -14,8 +17,10 @@ public class PreClickController {
     private PreClickService preClickService;
 
     @PostMapping ("/preClick")
-    public  Result<String> preClick(UserClick userClick){
-
+    public  Result<String> preClick(UserClick userClick, HttpSession session){
+        if(!UserUtil.checkUser(session)){
+            return new Result("-1","用户未登录");
+        }
     Result result=preClickService.preClick(userClick);
         return result;
     }
--- src/main/java/com/zyzx/redbag/controller/RankController.java
+++ src/main/java/com/zyzx/redbag/controller/RankController.java
@@ -3,11 +3,13 @@ package com.zyzx.redbag.controller;
 import com.zyzx.redbag.entry.Ranking;
 import com.zyzx.redbag.entry.Result;
 import com.zyzx.redbag.service.RankService;
+import com.zyzx.redbag.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -17,7 +19,10 @@ public class RankController {
     private RankService rankService;
     @RequestMapping("/getRank")
     @ResponseBody
-    public  Result getRank(){
+    public  Result getRank(HttpSession session){
+        if(!UserUtil.checkUser(session)){
+            return new Result("-1","用户未登录");
+        }
     Result result =rankService.getRank();
         return result;
     }
